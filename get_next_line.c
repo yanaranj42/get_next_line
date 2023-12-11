8776b337da8bf4cbb54794779f140dab0545a782
Yajaira Naranjo Gallo <yanaranj@car11s1.42barcelona.com>
2023-12-11 15:55:31 +0100

    gnl update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yanaranj <yanaranj@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 13:01:11 by yanaranj          #+#    #+#             */
-/*   Updated: 2023/12/11 15:04:56 by yanaranj         ###   ########.fr       */
+/*   Updated: 2023/12/11 15:54:51 by yanaranj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,51 +22,84 @@ static char	*ft_free(char *s1, char *s2)
 		free(s2);
 	return (NULL);
 }
-char	ft_line()
-{
-}
+
 char	ft_next()
 {
 }
+/* Leera la linea que debe retornar
+ * *line: cadena a devolver
+ * i: iterador de la cadena
+ * si el buffer tiene contenido y es distinto a \n, itera en el buffer
+ * Asigna memoria en line.
+ * while(): lee el buffer y comprueba si es diferente a '\n'
+ * 			sigue leyendo los caracteres del archivo.
+ * Asigna memoria en line y verifica en caso de error.
+ * while(): lee el buffer y comprueba si es diferente a '\n'
+ * 			copia el buffer en line.
+ * if(): cuando el buffer sea '\n'
+ * 		 se mueve en la pos de line y agrega el '\n' 
+*/
+char	ft_line(char *buffer)
+{
+	char	*line;
+	int		i;
 
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+//	while (buffer[i] && buffer[i] != '\n')
+//		i++;
+	line = malloc (size(char) * BUFFER_SIZE + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
 
-/* Leera la linea que encuentre en el FD y concatena las cadenas en el
+/* Leera en el FD y concatena las cadenas en el
  * STRG y devuelve el resultado de la cadena(strg).
- * *buf: almacena datos leido de manera temp.
+ * *buffer: almacena datos leido de manera temp.
  * byte: num de bytes que ira contando. Se inicia en 1 para entrar en
  *		 el bucle
- * malloc: para asignar memoria en el buf.
+ * malloc: para asignar memoria en el buffer.
  * while(): bucle que busca el caracter '\n' o hasta el final de linea
  * 			primero lee los bytes desde el fd al BS
  * 			Si se leyeron, primero asegura que que no termina en nulo.
- * 			Concatena el buf. a una cadena existente y lo almacena en
+ * 			Concatena el buffer a una cadena existente y lo almacena en
  * 			strg. Si falla, return NULL.
  * 			Else (si no ha leido bytes), libera los datos.
- * Al salir del bucle, libera la memoria de buf despues de leer las lineas
+ * Al salir del bucle, libera la memoria de buffer despues de leer las lineas
  */
-static char	*read_line(int fd, char *strg)
+static char	*read_file(int fd, char *strg)
 {
-	char	*buf;
+	char	*buffer;
 	int		bytes;
 
 	byte = 1;
-	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf)
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
 		return (ft_free(NULL, strg));
-	while (bytes > 0 && !ft_strchr(buf, '\n'))
+	while (bytes > 0 && !ft_strchr(buffer, '\n'))
 	{
-		bytes = read(fd, buf, BUFFER_SIZE);
+		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes > 0)
 		{
-			buf[bytes] = '\0';//iguala a nulo para que sea una cadena ok
-			strg = ft_strjoin(strg, buf);
+			buffer[bytes] = '\0';//iguala a nulo para que sea una cadena ok
+			strg = ft_strjoin(strg, buffer);
 			if (!strg)
 				return (NULL);
 		}
 		else if (bytes < 0)
-			return (ft_free(strg, buf));
+			return (ft_free(strg, buffer));
 	}
-	free(buf);
+	free(buffer);
 	return (strg);	
 }
 
